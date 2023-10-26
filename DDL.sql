8f527f6551fa072d51ea7ae223c4431acc4ac1c4
Enzo Ruiz Diaz <enzoruizdiaz10@gmail.com>
2023-10-26 18:51:22 -0300

    update del tipo de dato

--- DDL.sql
+++ DDL.sql
@@ -1,24 +1,24 @@
 
 -- Definición de la dimensión Desarrollador
 CREATE TABLE IF NOT EXISTS dimension_desarrollador (
-    desarrollador_id INT PRIMARY KEY,
+    desarrollador_id SERIAL PRIMARY KEY,
     nombre_desarrollador VARCHAR(255)
 );
 
 -- Definición de la dimensión Publicador
 CREATE TABLE IF NOT EXISTS dimension_publicador (
-    publicador_id INT PRIMARY KEY,
+    publicador_id SERIAL PRIMARY KEY,
     nombre_publicador VARCHAR(255)
 );
 
 -- Definición de la dimensión Genero
 CREATE TABLE IF NOT EXISTS dimension_genero (
-    genero_id INT PRIMARY KEY,
+    genero_id SERIAL PRIMARY KEY,
     nombre_genero VARCHAR(255)
 );
 -- Definición de la dimensión Juego
 CREATE TABLE IF NOT EXISTS dimension_juego (
-    juego_id INT PRIMARY KEY,
+    juego_id SERIAL PRIMARY KEY,
     nombre VARCHAR(255),
     desarrollador_id INT,
     publicador_id INT,
@@ -28,17 +28,25 @@ CREATE TABLE IF NOT EXISTS dimension_juego (
     FOREIGN KEY (genero_id) REFERENCES dimension_genero(genero_id)
 );
 
+-- Definición de la dimensión Compania del critico
+CREATE TABLE IF NOT EXISTS dimension_compania (
+    compania_id SERIAL PRIMARY KEY,
+    nombre_compania VARCHAR(255)
+);
+
+
 -- Definición de la dimensión Crítico
 CREATE TABLE IF NOT EXISTS dimension_critico (
-    critico_id INT PRIMARY KEY,
+    critico_id SERIAL PRIMARY KEY,
     nombre_critico VARCHAR(255),
     compania_critico VARCHAR(255)
     score_rating FLOAT
+    FOREIGN KEY (compania_id) REFERENCES dimension_compania(compania_id)
 );
 
 -- Definición de la dimensión Tiempo
 CREATE TABLE IF NOT EXISTS dimension_tiempo (
-    tiempo_id INT PRIMARY KEY,
+    tiempo_id SERIAL PRIMARY KEY,
     ano INT,
     mes INT,
     dia INT
@@ -46,7 +54,7 @@ CREATE TABLE IF NOT EXISTS dimension_tiempo (
 
 -- Definición de la dimensión de hecho Rating
 CREATE TABLE IF NOT EXISTS dimension_hecho_rating (
-    juego_id INT,
+    juego_id SERIAL,
     critico_id INT,
     tiempo_id INT,
     rating_promedio_publicador FLOAT,
